cf8999b7917f8833bd53500844d579997ec23aa7
mrlzmtl <mluizamnogueira@gmail.com>
2024-11-04 17:28:27 -0300

    Exercicio 9

---
+++ Assets/Script/Exercicio 09.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Exercicio09 : MonoBehaviour
+{
+
+    [SerializeField] int timeA = 0;
+    [SerializeField] int timeB = 0;
+    
+    void Start()
+    {
+        if (timeA > timeB)
+        {
+            print("Vitória do time A");
+        }
+        else if (timeA < timeB)
+        {
+            print("Vitória do time B");
+        }
+        else if (timeA == 3 && timeB == 3)
+        {
+            print("Empate emocionante");
+        }
+        else if (timeA == timeB)
+        {
+            print("Empate");
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
