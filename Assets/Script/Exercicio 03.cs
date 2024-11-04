d2e7a830377c6833b43a05e63c19e99f40ee9cd9
mrlzmtl <mluizamnogueira@gmail.com>
2024-11-04 15:44:25 -0300

    Exercicio 3

--- Assets/Script/Exercicio 03.cs
+++ Assets/Script/Exercicio 03.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Exercicio03 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] bool playerVivo;
+    [SerializeField] bool powerUp;
+    double vidaPlayer = 100;
+
     void Start()
     {
-        
+        if (playerVivo == false && powerUp == true)
+        {
+            vidaPlayer = vidaPlayer * 0.5;
+            print("Ainda não, vida atual" + vidaPlayer);
+        }
+        else 
+        {
+            print("Game Over");
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+
     }
 }
